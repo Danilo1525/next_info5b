9a373e21e752d0abbc0f2ec77c480250343a3de2
Danilo Martinez <126427304+Danilo1525@users.noreply.github.com>
2025-05-16 12:32:42 +0000

    feat: add loading GIFs and update useEffect component to display loading state

--- src/pages/useeffect.js
+++ src/pages/useeffect.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+
+
 export default function UseEffect() {
-  return (
-    <>
+  const [numero, setNumero] = useState(0);
+  setTimeout(() => {
+    setNumero(numero + 1);
+    }, 3000);
+
+    useEffect(() => {
+        if (numero > 0) {
+            document.title = `UseEffect ${numero}`;
+        }
+    }, [numero]);
+  return <>
       <div className="text-center">
         <h1>Use Effect</h1>
-        </div>
+        <h2>{numero === 0 ? <img src={"./loading-2.gif"} height={"40px"} width={"100px"}/> : numero}</h2>
+      </div>
     </>
-  );
 }
