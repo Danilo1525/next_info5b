5f1f8c218febdc83956a90ae75cd603bbc5c66a3
Danilo Martinez <126427304+Danilo1525@users.noreply.github.com>
2025-06-27 12:05:47 +0000

    feat: adicionar novos endpoints de API para filtragem de notícias e atualizar componentes de exibição

--- src/pages/api/noticias/[id].js
+++ src/pages/api/noticias/[id].js
@@ -1,6 +1,9 @@
-import {noticias} from "./noticias"
+import { noticias } from "./noticias"
 export default function handler(req, res) {
-
-    const encontrado = noticias.find(noticia => noticia.idnoticia.toString() === req.query.id)
+  const encontrado = noticias.find((noticia) =>
+    noticia.idnoticia.toString() === req.query.id);
+  if (!encontrado) {
+    return res.status(404).json({ error: "Notícia não encontrada" });
+  }
   res.status(200).json(encontrado)
 }
